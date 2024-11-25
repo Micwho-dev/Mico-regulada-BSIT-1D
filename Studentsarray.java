dfacc86d348864ac19777a171bef07738443086f
Micwho-dev <reguladanoelmico@gmail.com>
2024-11-25 21:06:37 +0800

    Add files via upload

---
+++ Studentsarray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		String[] students = {"John", "Alice", "Bob", "Eve"};
+        int[] scores = {85, 92, 76, 88};
+        for (int i = 0; i < students.length; i++) {
+        System.out.println(students[i] + "-" + scores[i]);
+	
+		}
+	}
+}
